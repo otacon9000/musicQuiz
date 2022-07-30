ac86d944fed591e6d72a347dafe512624d3ec8af
Andrea <andrea.alicino@gmail.com>
2022-07-30 11:46:17 -0400

    quiz logic work

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -4,26 +4,39 @@ using UnityEngine;
 
 public class UIManager : MonoSingleton<UIManager>
 {
-    [Header("UI Panel")] [SerializeField] private GameObject _welcomePanel;
-    [SerializeField] private GameObject _quizPanel;
-    [SerializeField] private GameObject _resultPanel;
+    [Header("UI Panel")] 
+    [SerializeField] 
+    private GameObject _welcomePanel;
+    [SerializeField] 
+    private GameObject _quizPanel;
+    [SerializeField] 
+    private GameObject _resultPanel;
 
     [Header("Question Panel")] 
+    [SerializeField] 
+    private GameObject _questionPanel;
     [SerializeField]
     private string _currentPlaylistName;
     private int _currentPlaylistIndex;
     [SerializeField] [Range(0, 4)]
-    private int _questionCounter;
+    private int _questionCounter = 0;
     [SerializeField] 
     private Question _currentQuestion;
-
+    
+    
     public void SetCurrentPlaylistName(int playlistIndex)
     {
         _currentPlaylistName = JsonImporter.Instance.GetPlaylistName(playlistIndex);
         _currentPlaylistIndex = playlistIndex;
+        
+        _welcomePanel.SetActive(false);
+        SetCurrentQuestion();
+        _quizPanel.SetActive(true);
+        //_questionPanel.SetActive(true);
+        
     }
 
-    public void SerCurrentQuestion()
+    public void SetCurrentQuestion()
     {
         _currentQuestion = JsonImporter.Instance.GetQuestion(_currentPlaylistIndex,_questionCounter);
     }
@@ -35,15 +48,40 @@ public class UIManager : MonoSingleton<UIManager>
     public void UpdateQuestionCounter()
     {
         _questionCounter++;
-        if (_questionCounter > 3)
+        if (_questionCounter > 4)
         {
             //go to result
+            _quizPanel.SetActive(false);
+            _resultPanel.SetActive(true);
             //set question counter to 0? 
+            _questionCounter = 0;
         }
         else
         {
-            
+            //go to next question or update field
+            SetCurrentQuestion();
+            _questionPanel.SetActive(false);
+            _questionPanel.SetActive(true);
         }
+    }
+    
+    //test to check with the correct answer
+    public void CheckAnswer(int buttonIndex)
+    {
+        if (buttonIndex != _currentQuestion.answerIndex)
+        {
+            //bad feedback
+            Debug.Log("wrong");
+            GameManager.Instance.AddResult(_questionCounter, false);
+        }
+        else
+        {
+            //good feedback
+            Debug.Log("correct");
+            GameManager.Instance.AddResult(_questionCounter, true);
+        }
+        //go to the next question
+        UpdateQuestionCounter();
     }
 
 }
--- Assets/Scripts/QuestionPanel.cs
+++ Assets/Scripts/QuestionPanel.cs
@@ -5,27 +5,22 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class QuestionPanel : MonoBehaviour, IPanel
-{
-    [SerializeField]
+{ 
     private Question _question;
-
-    [SerializeField] 
-    [Range(0,4)]
-    private int _index;
-
     public Text[] answerText;
     public void ProcessInfo()
     {
-        _question = JsonImporter.Instance.GetQuestion(GameManager.Instance.playlistSelected,_index);
-        //_question = JsonImporter.Instance.GetQuestion(0,0);
+        _question = UIManager.Instance.GetCurrentQuestion();
         for (int i = 0; i < _question.choices.Length; i++)
         {
             answerText[i].text = _question.choices[i].artist +"\n" +_question.choices[i].title;
         }
     }
-
-    private void Start()
+    //non sará fatto nello start ma da un onclick event 
+    private void OnEnable()
     {
         ProcessInfo();
     }
+    
+   
 }
